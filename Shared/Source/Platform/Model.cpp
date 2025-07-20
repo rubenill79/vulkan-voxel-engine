71a85cd5720ee5947cd698e38cc91571bf233745
Rubén Lázaro <rubenill79@hotmail.com>
2025-07-20 18:48:00 +0200

    Added models with index buffer support using tinyobjloader

--- Shared/Source/Platform/Model.cpp
+++ Shared/Source/Platform/Model.cpp
@@ -1,24 +1,64 @@
 #include "Model.hpp"
 
+#include "Utils/Utils.hpp"
+
+// libs
+#define TINYOBJECTLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
+// std
 #include <cassert>
+#include <cstring>
+#include <iostream>
+#include <unordered_map>
 
-namespace VoxelEngine {
+namespace std
+{
+    template <>
+    struct hash<VoxelEngine::Model::Vertex>
+    {
+        size_t operator()(VoxelEngine::Model::Vertex const &vertex) const
+        {
+            size_t seed = 0;
+            VoxelEngine::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+            return seed;
+        }
+    };
+}
 
-    Model::Model(Device &device, Model::Builder &builder): device{device} {
-        createVertexBuffer(builder.vertices);
-        createIndexBuffer(builder.indices);
+namespace VoxelEngine
+{
+
+    Model::Model(Device &device, const Model::Builder &builder) : device{device}
+    {
+        createVertexBuffers(builder.vertices);
+        createIndexBuffers(builder.indices);
     }
 
-    Model::~Model() {
+    Model::~Model()
+    {
         vkDestroyBuffer(device.device(), vertexBuffer, nullptr);
         vkFreeMemory(device.device(), vertexBufferMemory, nullptr);
-        if (hasIndexBuffer) {
+        if (hasIndexBuffer)
+        {
             vkDestroyBuffer(device.device(), indexBuffer, nullptr);
             vkFreeMemory(device.device(), indexBufferMemory, nullptr);
         }
     }
 
-    void Model::createVertexBuffer(const std::vector<Vertex> &vertices) {
+    std::unique_ptr<Model> Model::createModelFromFile(
+        Device &device, const std::string &filePath)
+    {
+        Builder builder{};
+        builder.loadModel(filePath);
+        std::cout << "Vertex count: " << builder.vertices.size() << std::endl;
+        return std::make_unique<Model>(device, builder);
+    }
+
+    void Model::createVertexBuffers(const std::vector<Vertex> &vertices)
+    {
         vertexCount = static_cast<uint32_t>(vertices.size());
         assert(vertexCount >= 3 && "Vertex count must be ");
         VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
@@ -50,12 +90,14 @@ namespace VoxelEngine {
         vkFreeMemory(device.device(), stagingBufferMemory, nullptr);
     }
 
-    void Model::createIndexBuffer(const std::vector<uint32_t> &indices) {
+    void Model::createIndexBuffers(const std::vector<uint32_t> &indices)
+    {
         indexCount = static_cast<uint32_t>(indices.size());
         hasIndexBuffer = indexCount > 0;
-        if (!hasIndexBuffer) return;
+        if (!hasIndexBuffer)
+            return;
         VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
-        
+
         VkBuffer stagingBuffer;
         VkDeviceMemory stagingBufferMemory;
         device.createBuffer(
@@ -83,29 +125,32 @@ namespace VoxelEngine {
         vkFreeMemory(device.device(), stagingBufferMemory, nullptr);
     }
 
-    void Model::bind(VkCommandBuffer commandBuffer) {
+    void Model::bind(VkCommandBuffer commandBuffer)
+    {
         VkBuffer buffers[] = {vertexBuffer};
         VkDeviceSize offsets[] = {0};
         vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
 
-        if (hasIndexBuffer) 
+        if (hasIndexBuffer)
         {
             vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT32);
-        }  
+        }
     }
 
-    void Model::draw(VkCommandBuffer commandBuffer) {
+    void Model::draw(VkCommandBuffer commandBuffer)
+    {
         if (hasIndexBuffer)
         {
             vkCmdDrawIndexed(commandBuffer, indexCount, 1, 0, 0, 0);
-        } 
+        }
         else
         {
             vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
         }
     }
 
-    std::vector<VkVertexInputBindingDescription> Model::Vertex::getBindingDescriptions() {
+    std::vector<VkVertexInputBindingDescription> Model::Vertex::getBindingDescriptions()
+    {
         std::vector<VkVertexInputBindingDescription> bindingDescriptions(1);
         bindingDescriptions[0].binding = 0;
         bindingDescriptions[0].stride = sizeof(Vertex);
@@ -113,13 +158,14 @@ namespace VoxelEngine {
         return bindingDescriptions;
     }
 
-    std::vector<VkVertexInputAttributeDescription> Model::Vertex::getAttributeDescriptions() {
+    std::vector<VkVertexInputAttributeDescription> Model::Vertex::getAttributeDescriptions()
+    {
         std::vector<VkVertexInputAttributeDescription> attributeDescriptions(2);
         attributeDescriptions[0].binding = 0;
         attributeDescriptions[0].location = 0;
         attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[0].offset = offsetof(Vertex, position);
-        
+
         attributeDescriptions[1].binding = 0;
         attributeDescriptions[1].location = 1;
         attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
@@ -127,4 +173,75 @@ namespace VoxelEngine {
         return attributeDescriptions;
     }
 
+    void Model::Builder::loadModel(const std::string &filepath)
+    {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn, err;
+
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str()))
+        {
+            throw std::runtime_error(warn + err);
+        }
+
+        vertices.clear();
+        indices.clear();
+
+        std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+        for (const auto &shape : shapes)
+        {
+            for (const auto &index : shape.mesh.indices)
+            {
+                Vertex vertex{};
+
+                if (index.vertex_index >= 0)
+                {
+                    vertex.position = {
+                        attrib.vertices[3 * index.vertex_index + 0],
+                        attrib.vertices[3 * index.vertex_index + 1],
+                        attrib.vertices[3 * index.vertex_index + 2],
+                    };
+
+                    auto colorIndex = 3 * index.vertex_index + 2;
+                    if (colorIndex < attrib.colors.size())
+                    {
+                        vertex.color = {
+                            attrib.colors[colorIndex - 2],
+                            attrib.colors[colorIndex - 1],
+                            attrib.colors[colorIndex - 0],
+                        };
+                    }
+                    else
+                    {
+                        vertex.color = {1.f, 1.f, 1.f}; // set default color
+                    }
+                }
+
+                if (index.normal_index >= 0)
+                {
+                    vertex.normal = {
+                        attrib.normals[3 * index.normal_index + 0],
+                        attrib.normals[3 * index.normal_index + 1],
+                        attrib.normals[3 * index.normal_index + 2],
+                    };
+                }
+
+                if (index.texcoord_index >= 0)
+                {
+                    vertex.uv = {
+                        attrib.texcoords[2 * index.texcoord_index + 0],
+                        attrib.texcoords[2 * index.texcoord_index + 1],
+                    };
+                }
+
+                if (uniqueVertices.count(vertex) == 0)
+                {
+                    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(uniqueVertices[vertex]);
+            }
+        }
+    }
 }
